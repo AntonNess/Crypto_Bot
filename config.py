4011194295945e4b98f96a23c267c33bb53282ed
AntonNess <98344191+AntonNess@users.noreply.github.com>
2022-03-02 19:17:51 +0600

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = '5295670536:AAFucxB-Lmx9_8TQxxiEfOnRok1bxrcTOMw'
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'тенге': 'KZT',
+    'рубль': 'RUB',
+    'лира': 'TRY',
+    'евро': 'EUR',
+    'фунты': 'GBP',
+    'юань': 'CNY',
+    'иена': 'JPY'
+}
